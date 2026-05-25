32f8763ee5e206db7994d6895550125cb558aac9
Stéphane Billeter <stephane.billeter@unil.ch>
2026-05-26 01:12:34 +0200

    add wrapper to get https to fastapi

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -6,11 +6,12 @@ django.setup()
 
 from django.core.asgi import get_asgi_application
 from api.main import app as fastapi_app
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
 _django_app = get_asgi_application()
 
 
-async def application(scope, receive, send):
+async def _application(scope, receive, send):
     path = scope.get("path", "")
     if scope["type"] == "http" and (
         path.startswith("/api/")
@@ -19,3 +20,6 @@ async def application(scope, receive, send):
         await fastapi_app(scope, receive, send)
     else:
         await _django_app(scope, receive, send)
+
+
+application = ProxyHeadersMiddleware(_application, trusted_hosts="*")
